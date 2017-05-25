36073e132e9852e7029cb4cb04ee751e0d32a6fb
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-05-25 10:17:09 +0300

    show ECO link for footnote 05002

--- app/models/footnote.rb
+++ app/models/footnote.rb
@@ -3,9 +3,15 @@ require 'api_entity'
 class Footnote
   include ApiEntity
 
+  ECO_CODE = '05002'
+
   attr_accessor :code, :description, :formatted_description
 
   def id
     @id ||= "#{casted_by.destination}-#{casted_by.id}-footnote-#{code}"
+  end
+
+  def eco?
+    code == ECO_CODE
   end
 end
