64b8153daabe0f9251e7862fa8a7d0f36e57d7b3
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-10-13 16:22:44 +0300

    removing measure.key method (used for measures ordering on frontend)

--- spec/models/measure_spec.rb
+++ spec/models/measure_spec.rb
@@ -69,34 +69,4 @@ describe Measure do
       ).to eq false
     end
   end
-
-  describe '#key' do
-    it 'has 0 in the 0 position for national' do
-      measure = Measure.new(
-        attributes_for(:measure, :national, geographical_area: {id: '1234', description: 'Other'})
-      )
-      expect(measure.key[0]).to eq('0')
-    end
-
-    it 'has 1 in the 0 position for not national' do
-      measure = Measure.new(
-        attributes_for(:measure, :eu, geographical_area: {id: '1234', description: 'Other'})
-      )
-      expect(measure.key[0]).to eq('1')
-    end
-
-    it 'has 0 in the 1 position for vat' do
-      measure = Measure.new(
-        attributes_for(:measure, :eu, vat: true, geographical_area: {id: '1234', description: 'Other'})
-      )
-      expect(measure.key[1]).to eq('0')
-    end
-
-    it 'has 1 in the 1 position for not vat' do
-      measure = Measure.new(
-        attributes_for(:measure, :eu, geographical_area: {id: '1234', description: 'Other'})
-      )
-      expect(measure.key[1]).to eq('1')
-    end
-  end
 end
