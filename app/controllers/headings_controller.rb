aa4aac231a3a8a23c973f51bc4b6be0eb8ba8af4
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-12-06 13:23:20 +0300

    using chapter_path and and heading_path as a back link href

--- app/controllers/headings_controller.rb
+++ app/controllers/headings_controller.rb
@@ -2,6 +2,6 @@ class HeadingsController < GoodsNomenclaturesController
   def show
     @heading = HeadingPresenter.new(Heading.find(params[:id], query_params))
     @commodities = HeadingCommodityPresenter.new(@heading.commodities)
-    @back_path = "#{chapters_path}/#{@heading.chapter.short_code}"
+    @back_path = chapter_path(@heading.chapter.short_code)
   end
 end
