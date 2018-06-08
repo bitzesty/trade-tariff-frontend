e653def84afc2c5073ea7183c6cd90c06204139d
Ariel <amonaco@gmail.com>
2018-06-08 11:01:27 +0100

    adding filter for nihil

--- app/helpers/measures_helper.rb
+++ app/helpers/measures_helper.rb
@@ -2,4 +2,8 @@ module MeasuresHelper
   def order_number_info(record)
     record.definition.try(:description) || "Order number #{record.number}"
   end
+
+  def filter_duty_expression(record)
+    (record.include? 'NIHIL') ? '' : record
+  end
 end
