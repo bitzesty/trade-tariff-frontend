93935d803e02e87f0ff45a91c4fa1c3442ed838b
Ariel <amonaco@users.noreply.github.com>
2018-06-12 04:37:22 -0500

    [TARIFF17] Hide duty expression ID = '37', at the moment it displays as NIHIL (#197)
    * adding filter for nihil

--- app/helpers/measures_helper.rb
+++ app/helpers/measures_helper.rb
@@ -2,4 +2,8 @@ module MeasuresHelper
   def order_number_info(record)
     record.definition.try(:description) || "Order number #{record.number}"
   end
+
+  def filter_duty_expression(record)
+    record == 'NIHIL' ? '' : record
+  end
 end
