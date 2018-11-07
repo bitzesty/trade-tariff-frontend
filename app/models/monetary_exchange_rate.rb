dd30bec48ecaafe8c1eea641ca1160a947ca7ae8
Maurício André Cinelli <mauricio.cinelli@bitzesty.com>
2018-11-07 10:58:56 -0200

    Building exchange rates tables

---
+++ app/models/monetary_exchange_rate.rb
@@ -0,0 +1,20 @@
+require 'api_entity'
+
+class MonetaryExchangeRate
+  include ApiEntity
+
+  collection_path "/monetary_exchange_rates"
+
+  attr_accessor :child_monetary_unit_code,
+                :exchange_rate,
+                :operation_date,
+                :validity_start_date
+
+  def validity_start_date
+    DateTime.parse(@validity_start_date)
+  end
+
+  def inverse_rate
+    (1/exchange_rate.to_f).to_d.truncate(9)
+  end
+end
--- lib/trade_tariff_frontend.rb
+++ lib/trade_tariff_frontend.rb
@@ -10,7 +10,7 @@ module TradeTariffFrontend
   # API Endpoints of the Tariff API app that can be reached
   # via Frontend
   def accessible_api_endpoints
-    ['sections', 'chapters', 'headings', 'commodities', 'updates']
+    ['sections', 'chapters', 'headings', 'commodities', 'updates', "monetary_exchange_rates"]
   end
 
   def production?
