bf8e205fb33a1fabac47e34d716b9bb1eefcf6dc
Alexey Klimuk <alexey.klimuk@gmail.com>
2018-11-15 16:46:41 +0100

    Show 10 or 8 numbers for commodity codes

--- app/helpers/commodities_helper.rb
+++ app/helpers/commodities_helper.rb
@@ -40,21 +40,11 @@ module CommoditiesHelper
 
   def format_commodity_code_based_on_level(commodity)
     code = commodity.code.to_s
-    is_declarable = (
-      commodity.number_indents == 1 &&
-      (
-        commodity.leaf? ||
-        commodity.producline_suffix == '80'
-      )
-    )
+    display_full_code = commodity.producline_suffix == '80'
 
-    if commodity.number_indents > 1 || is_declarable
-      code = if code[6..9] == "0000"
-        code[0..5]
-      elsif code[8..9] == "00"
-        code[0..7]
-      else
-        code
+    if commodity.number_indents > 1 || display_full_code
+      if commodity.has_children?
+        code = code[0..7]
       end
 
       "#{chapter_and_heading_codes(code)}
