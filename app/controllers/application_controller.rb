6b91c535a1c1c13f131ac8a5f21473e9f87d45eb
myxrome <myxrome@gmail.com>
2019-02-25 16:49:14 +0400

    added basic http authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   include TradeTariffFrontend::ViewContext::Controller
   include ApplicationHelper
 
+  http_basic_authenticate_with name: ENV['CDS_USER'], password: ENV['CDS_PASSWORD'], if: -> { ENV['CDS_LOCKED'].present? && ENV['CDS_USER'].present? && ENV['CDS_PASSWORD'].present? }
   before_action :set_last_updated
   before_action :set_cache
   before_action :preprocess_raw_params
