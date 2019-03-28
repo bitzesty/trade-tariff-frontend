5d7e87653ebb6a0fa2085b553b68938194974637
Matthew Ford <matt@bitzesty.com>
2019-03-28 15:50:36 +0000

    Allow upgrading of assets version

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = ENV["ASSETS_VERSION"] || '1.0'
 
 # Enable the asset pipeline
 Rails.application.config.assets.enabled = true
