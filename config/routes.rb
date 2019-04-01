71ac7bf95e66ae4ec29dc361f523349b2e929b5a
Mario Andrés Correa <a@macool.me>
2019-03-31 21:15:56 -0500

    lograge - log 404s properly

--- config/routes.rb
+++ config/routes.rb
@@ -82,4 +82,5 @@ Rails.application.routes.draw do
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
   match "/503", to: "errors#maintenance", via: :all
+  match "*path", to: "errors#not_found", via: :all
 end
