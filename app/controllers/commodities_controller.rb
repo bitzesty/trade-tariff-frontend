8c7ff004d931e28856d5ee75c38add1197bd6a88
Christopher Unger <mcu@mac.com>
2019-05-16 12:48:06 -0400

    Calculate previous and next commodities within a heading

--- app/controllers/commodities_controller.rb
+++ app/controllers/commodities_controller.rb
@@ -1,10 +1,11 @@
 class CommoditiesController < GoodsNomenclaturesController
   def show
     @commodity = CommodityPresenter.new(Commodity.find(params[:id], query_params))
-    @heading = @commodity.heading
+    @heading = HeadingPresenter.new(Heading.find(@commodity.heading.short_code, query_params))
     @chapter = @commodity.chapter
     @section = @commodity.section
     @back_path = request.referer || heading_path(@heading.short_code)
+    @commodity.prev, @commodity.next = set_prev_next
   end
 
   private
@@ -12,5 +13,14 @@ class CommoditiesController < GoodsNomenclaturesController
   def commodities_by_code
     search_term = Regexp.escape(params[:term].to_s)
     Commodity.by_code(search_term).sort_by(&:code)
+  end
+
+  def set_prev_next(heading = @heading, commodity = @commodity)
+    gnids = heading.commodities.select(&:leaf?).map(&:goods_nomenclature_item_id)
+    i = gnids.index(commodity.goods_nomenclature_item_id)
+    return [
+      i == 0 ? nil : gnids[i-1],
+      i == (gnids.length - 1) ? nil : gnids[i+1]
+    ]
   end
 end
