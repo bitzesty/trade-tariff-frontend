c870a6da3ef9c4724f7d62fca752801313c086c2
Christopher Unger <mcu@mac.com>
2019-06-14 12:06:12 -0400

    Adding specs for new functionality

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -21,5 +21,33 @@ describe ApplicationHelper, type: :helper do
         ).to eq "alert('hello');"
       end
     end
+
+    context 'HashWithIndifferentAccess is passed as argument' do
+      let(:hash) {
+        {"content"=>"* 1\\. This chapter does not cover:"}.with_indifferent_access
+      }
+
+      it 'fetches :content from the hash' do
+        expect(
+          helper.govspeak(hash)
+        ).to eq <<~FOO
+        <ul>
+          <li>1. This chapter does not cover:</li>
+        </ul>
+        FOO
+      end
+    end
+
+    context 'HashWithIndifferentAccess is passed as argument with no applicable content' do
+      let(:na_hash) {
+        {"foo"=>"bar"}.with_indifferent_access
+      }
+
+      it 'returns an empty string' do
+        expect(
+          helper.govspeak(na_hash)
+        ).to eq ''
+      end
+    end
   end
 end
