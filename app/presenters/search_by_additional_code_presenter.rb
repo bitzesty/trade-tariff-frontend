9a1388736177e02e82f32b5fd386db27dd84c2c6
Aleksey Zolotov <myxrome@gmail.com>
2019-08-06 17:14:27 +0400

    added checks if additional code has valid measure while search result rendering.

--- app/presenters/search_by_additional_code_presenter.rb
+++ app/presenters/search_by_additional_code_presenter.rb
@@ -4,7 +4,9 @@ class SearchByAdditionalCodePresenter
   def initialize(search_form)
     @search_form = search_form
     @search_result = AdditionalCode.search(search_form.to_params).sort_by do |additional_code|
-      additional_code&.measure&.goods_nomenclature&.goods_nomenclature_item_id
+      id = additional_code&.measure&.goods_nomenclature&.goods_nomenclature_item_id || ''
+      # move empty values to the end
+      [id.empty? ? 1 : 0, id]
     end if search_form.present?
   end
 end
