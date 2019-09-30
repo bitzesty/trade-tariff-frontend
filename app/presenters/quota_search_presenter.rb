9c5721972f21f34992f1831bf5d7bc1ae5c839a4
Aleksey Zolotov <myxrome@gmail.com>
2019-09-30 16:52:25 +0400

    added the error handling for additional code, certificate and footnote searches.

--- app/presenters/quota_search_presenter.rb
+++ app/presenters/quota_search_presenter.rb
@@ -4,10 +4,8 @@ class QuotaSearchPresenter
   def initialize(search_form)
     @with_errors = false
     @search_form = search_form
-    begin
-      @search_result = OrderNumber::Definition.search(search_form.to_params).sort_by(&:quota_order_number_id) if search_form.present?
-    rescue StandardError
-      @with_errors = true
-    end
+    @search_result = OrderNumber::Definition.search(search_form.to_params).sort_by(&:quota_order_number_id) if search_form.present?
+  rescue StandardError
+    @with_errors = true
   end
 end
