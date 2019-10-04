dd0edab9db7bb12b0872e69ace54165d8c4fd620
Aleksey Zolotov <myxrome@gmail.com>
2019-10-04 16:38:34 +0400

    removed sorting for search results.

--- app/presenters/certificate_search_presenter.rb
+++ app/presenters/certificate_search_presenter.rb
@@ -4,7 +4,7 @@ class CertificateSearchPresenter
   def initialize(search_form)
     @with_errors = false
     @search_form = search_form
-    @search_result = Certificate.search(search_form.to_params).sort_by(&:code) if search_form.present?
+    @search_result = Certificate.search(search_form.to_params) if search_form.present?
   rescue StandardError
     @with_errors = true
   end
