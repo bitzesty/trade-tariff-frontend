3f454f8554a22636751e44079a095dc42a95b87b
Aleksey Zolotov <myxrome@gmail.com>
2020-01-30 16:28:14 +0400

    fixed concerns namespaces.

--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -1,10 +1,8 @@
 require 'api_entity'
-require 'declarable'
-require 'changeable'
 
 class Commodity < GoodsNomenclature
-  include Models::Changeable
-  include Models::Declarable
+  include Changeable
+  include Declarable
 
   collection_path "/commodities"
 
