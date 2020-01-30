7a50d2cb7619d02b8609e41bc1b8ed1007bccb8f
Aleksey Zolotov <myxrome@gmail.com>
2020-01-30 15:43:49 +0400

    executed and merged `app:update`.

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,15 @@
 require_relative 'boot'
 
+require "rails"
+# Pick the frameworks you want:
+# require "active_model/railtie"
+# require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+require "action_view/railtie"
+# require "action_cable/engine"
 require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -13,7 +21,7 @@ APP_SLUG = 'trade-tariff'
 module TradeTariffFrontend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     require 'trade_tariff_frontend'
 
