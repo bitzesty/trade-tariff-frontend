c43a53445c2cfeb418daed874c9cd454479a1755
Alexey Klimuk <alexey.klimuk@bitzesty.com>
2020-12-06 00:11:32 +0100

    Add feedback params validation

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -6,14 +6,29 @@ class FeedbackController < ApplicationController
   def new; end
 
   def create
-    FrontendMailer.new_feedback(params[:message], params[:name], params[:email]).deliver_now
+    feedback = Feedback.new(feedback_params)
+
+    if feedback.valid?
+      FrontendMailer.new_feedback(feedback).deliver_now
+      status = :ok
+      flash[:alert] = 'Thanks you for your feedback.'
+    else
+      flash[:alert] = 'Something went wrong.'
+      status = :unprocessable_entity
+    end
 
     if request.xhr?
-      head :ok
+      head status
     else
       redirect_to action: :thanks
     end
   end
 
   def thanks; end
+
+  private
+
+  def feedback_params
+    params.permit(:message, :name, :email)
+  end
 end
