1387bddfeaf641f858a152083fa2c96e3bfbf824
William Fish <william.michael.fish@gmail.com>
2020-12-08 16:14:37 +0000

    Remove unused api host in all rails environments

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,7 +37,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  # Host for Trade Tariff API endpoint
-  config.api_host = "http://localhost:3018"
 end
