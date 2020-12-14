06634a02f819da3e1dcb18fe7239d6346f0ba6f9
William Fish <william.michael.fish@gmail.com>
2020-12-14 17:40:22 +0000

    Adds dynamic service heading
    jira link: https://transformuk.atlassian.net/browse/HOTT-82
    
    Adds a new heading for uk and xi services and consume them in the
    basey layout
    
    Add feature spec to show the heading behaviour when:
    
    1. In default mode
    2. In uk mode
    3. In uk-old mode
    4. In xi mode

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def trade_tariff_heading
+    service_choice =
+      TradeTariffFrontend::ServiceChooser.service_choice ||
+      TradeTariffFrontend::ServiceChooser::SERVICE_DEFAULT
+
+    t('trade_tariff_heading')[service_choice.to_sym]
+  end
+
   def govspeak(text)
     text = text['content'] || text[:content] if text.is_a?(Hash)
     return '' if text.nil?
