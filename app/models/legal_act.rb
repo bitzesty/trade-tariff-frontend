798ecdb51c42e76a38b1438e7154fd509aa08c72
Aleksey Zolotov <myxrome@gmail.com>
2020-12-15 16:45:32 +0400

    added national data presence checks.

--- app/models/legal_act.rb
+++ app/models/legal_act.rb
@@ -7,14 +7,17 @@ class LegalAct
                 :validity_start_date, :published_date, :officialjournal_number, :officialjournal_page, :information_text
 
   def national_public_id
+    return nil unless has_national_data?
     information_text&.split('|')&.first
   end
 
   def national_url
+    return nil unless has_national_data?
     information_text&.split('|')&.fetch(1)
   end
 
   def national_information
+    return nil unless has_national_data?
     information_text&.split('|')&.last
   end
 
@@ -28,5 +31,11 @@ class LegalAct
 
   def url_safe_code
     regulation_code.tr("/", "_")
+  end
+
+  private
+
+  def has_national_data?
+    (information_text&.split('|')&.length || 0) == 3
   end
 end
