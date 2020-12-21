cbe343e04ac017454499915fc1f33c60af8f9c2d
Matthew Ford <matt@bitzesty.com>
2020-12-21 19:50:50 +0000

    Allow setting a simulation date to default the view at a specific date in time (#279)
    This will change the default date to be the SIMULATION_DATE.
    
    Also fixes a typo in currency options.

--- app/models/tariff_date.rb
+++ app/models/tariff_date.rb
@@ -14,6 +14,8 @@ class TariffDate
     new(
       if valid_date_param?(date_param)
         date_param.values_at(*DATE_KEYS).join('-')
+      elsif TradeTariffFrontend.simulation_date
+        TradeTariffFrontend.simulation_date
       else
         Date.current
       end
@@ -35,7 +37,7 @@ class TariffDate
   end
 
   def date
-    @date.presence || TariffUpdate.latest_applied_import_date
+    @date.presence || TradeTariffFrontend.simulation_date || TariffUpdate.latest_applied_import_date
   end
 
   def persisted?
